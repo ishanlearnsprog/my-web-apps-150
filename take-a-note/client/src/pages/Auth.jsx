e5600d065e6cd1c774bafc5b2501adcb3734b45f
Ishan Rajendra <learnprog21@gmail.com>
2023-05-15 17:53:25 +0530

    Added client side vaidations

--- take-a-note/client/src/pages/Auth.jsx
+++ take-a-note/client/src/pages/Auth.jsx
@@ -1,4 +1,5 @@
 import { useContext, useState } from "react";
+import validator from "validator";
 
 import { useUsersContext } from "../hooks/useUsersContext.jsx"
 import { signInUser, signUpUser } from "../api.jsx";
@@ -10,37 +11,61 @@ const Auth = () => {
     const [error, setError] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
 
+    const clear = () => {
+        setFormData({ email: "", password: "" });
+        setConfirmPassword("");
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+
         if (formData.email === "" || formData.password === "") {
             setError("All fields must be filled");
             return
         }
+
+        if (!validator.isEmail(formData.email)) {
+            setError("Email not valid");
+            return
+        }
+
         let res;
+
         if (isSignUp) {
+
             if (formData.password !== confirmPassword) {
-                setFormData({ email: "", password: "" });
-                setConfirmPassword("");
                 setError("Passwords do not match");
+                clear()
                 return;
             }
+
+            if (!validator.isStrongPassword(formData.password)) {
+                setError("Password not strong enough");
+                clear();
+                return
+            }
+
             res = await signUpUser({ ...formData })
                 .catch((error) => {
-                    setError(error.response.data.error);
-                    setFormData({ email: "", password: "" });
-                    setConfirmPassword("");
+                    setError(error?.response?.data?.error);
+                    clear()
                 });
+
         } else {
+
             res = await signInUser({ ...formData })
                 .catch((error) => {
-                    setError(error.response.data.error);
-                    setFormData({ email: "", password: "" });
+                    setError(error?.response?.data?.error);
+                    clear()
                 });
+
         }
+
         if (res?.status === 200) {
             localStorage.setItem("user", JSON.stringify(res.data));
             dispatch({ type: "LOGIN", payload: JSON.stringify(res.data) });
         }
+
     }
 
     return (
@@ -48,7 +73,7 @@ const Auth = () => {
             <section>
                 <h1>Take-A-Note</h1>
                 {error && (<p>{error}</p>)}
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} noValidate>
                     <div>
                         <label htmlFor="email">Email</label>
                         <input type="email" name="email" id="email" value={formData.email} onChange={(e) => (setFormData({ ...formData, email: e.target.value }))} />
